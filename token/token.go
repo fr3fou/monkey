2ee3ec94ff04d6f0989968d06fb595c05c2b09f1
Simo Aleksandrov <simo3003@me.com>
2019-08-04 01:13:55 +0300

    End of chapter 1.2 - Begin working on token.go

---
+++ token/token.go
@@ -0,0 +1,42 @@
+package token
+
+// All possible token variants
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// Type is used to determine the token variant
+type Type string
+
+// Token is the data structure that holds
+// all the necessary information that our
+// lexer is going to output
+type Token struct {
+	Type    Type
+	Literal string // This can be optimized by changing it to byte or int
+}
