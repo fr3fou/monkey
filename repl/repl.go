e2e85cb1dd5eac4de0870188a2dfec4f7cdeb3e4
Simo Aleksandrov <simo3003@me.com>
2019-08-04 22:31:10 +0200

    End of chapter 1 (Begin working on REPL)

---
+++ repl/repl.go
@@ -0,0 +1,29 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/fr3fou/monkey/token"
+
+	"github.com/fr3fou/monkey/lexer"
+)
+
+const prompt = ">> "
+
+// Start takes in an io.Reader and an io.Writer
+// and starts prompting inside and writing to it
+func Start(r io.Reader, w io.Writer) {
+	scanner := bufio.NewScanner(r)
+
+	fmt.Print(prompt)
+	for scanner.Scan() {
+		l := lexer.NewLexer(scanner.Text())
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(w, "%+v\n", tok)
+		}
+		fmt.Fprint(w, prompt)
+	}
+}
