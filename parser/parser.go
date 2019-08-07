7d45a5248897e8424869c19eb6c37c52dd70591e
Simo Aleksandrov <simo3003@me.com>
2019-08-08 00:39:24 +0200

    Parse identifier expressions

--- parser/parser.go
+++ parser/parser.go
@@ -8,12 +8,31 @@ import (
 	"github.com/fr3fou/monkey/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
+// Precendence order
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
+
 // Parser is the struct which does all of the parsing
 type Parser struct {
-	l       *lexer.Lexer
-	tok     token.Token
-	nextTok token.Token
-	errors  []string
+	l              *lexer.Lexer
+	tok            token.Token
+	nextTok        token.Token
+	errors         []string
+	prefixParseFns map[token.Type]prefixParseFn
+	infixParseFns  map[token.Type]infixParseFn
 }
 
 // New returns a pointer to a parser
@@ -26,6 +45,9 @@ func New(l *lexer.Lexer) *Parser {
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
+
+	p.prefixParseFns = make(map[token.Type]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
 
 	return p
 }
@@ -69,10 +91,11 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
+// parseLetStatement parses any let statment (let foo = 5)
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{
 		Token: p.tok,
@@ -101,6 +124,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+// parseReturnStatement parses any return statment (return '5')
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{
 		Token: p.tok,
@@ -118,6 +142,44 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
+// parseExpressionStatement parses any expression statements (foobar;)
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{
+		Token: p.tok,
+	}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.nextTokIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// parseExpression is a general function for parsing expression
+// it checks if the current token type has a matching function in our
+// map for prefix / infix expressions
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.tok.Type]
+
+	if prefix == nil {
+		return nil
+	}
+
+	leftExp := prefix()
+
+	return leftExp
+}
+
+// parseIdentifier parses any identifier (variable)
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{
+		Token: p.tok,
+		Value: p.tok.Literal,
+	}
+}
+
 // curTokenIs is a helper function that checks if the current
 // token type matches the one provided
 func (p *Parser) curTokenIs(t token.Type) bool {
@@ -130,6 +192,8 @@ func (p *Parser) nextTokIs(t token.Type) bool {
 	return p.nextTok.Type == t
 }
 
+// expectPeek is a helper function that checks if the next
+// token type matches the one provided and if yes, it calls p.nextToken()
 func (p *Parser) expectPeek(t token.Type) bool {
 	if p.nextTokIs(t) {
 		p.nextToken()
@@ -146,4 +210,16 @@ func (p *Parser) peekError(t token.Type) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
 		t, p.nextTok.Type)
 	p.errors = append(p.errors, msg)
+}
+
+// registerPrefix is a helper function that adds the provided function in the map
+// for prefix functions
+func (p *Parser) registerPrefix(tokenType token.Type, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+// registerInfix is a helper function that adds the provided function in the map
+// for infix functions
+func (p *Parser) registerInfix(tokenType token.Type, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
