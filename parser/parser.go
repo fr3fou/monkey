91f4d6662057a280e98df69fcb5a2d7b807e8a65
Simo Aleksandrov <simo3003@me.com>
2019-08-10 14:55:45 +0200

    Add newline

--- parser/parser.go
+++ parser/parser.go
@@ -209,8 +209,8 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 	precedence := p.curPrecedence()
 	p.nextToken()
-
 	expression.Right = p.parseExpression(precedence)
+
 	return expression
 
 }
