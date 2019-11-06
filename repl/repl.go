a474b48e7417fd1b87a282b6d18be26f7fddbdf9
Simo Aleksandrov <simo3003@me.com>
2019-11-06 23:54:22 +0200

    finished parser, implemented RPPL, now onto fixing tests...

--- repl/repl.go
+++ repl/repl.go
@@ -1,11 +1,10 @@
 package repl
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 
-	"github.com/fr3fou/monkey/token"
+	"github.com/fr3fou/monkey/parser"
 
 	"github.com/fr3fou/monkey/lexer"
 )
@@ -15,15 +14,26 @@ const prompt = ">> "
 // Start takes in an io.Reader and an io.Writer
 // and starts prompting inside and writing to it
 func Start(r io.Reader, w io.Writer) {
-	scanner := bufio.NewScanner(r)
 
 	fmt.Fprint(w, prompt)
-	for scanner.Scan() {
-		l := lexer.New(scanner.Text())
+	line := "1 + (2+3) + 4"
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(w, "%+v\n", tok)
-		}
-		fmt.Fprint(w, prompt)
+	l := lexer.New(line)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+
+	if len(p.Errors()) != 0 {
+		printParserErrors(w, p.Errors())
+	}
+
+	io.WriteString(w, program.String())
+	io.WriteString(w, "\n")
+	fmt.Fprint(w, prompt)
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
