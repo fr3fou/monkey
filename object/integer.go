c2b5560292eea9fce1391d5e7f94b5140a01ee72
Simo Aleksandrov <simo3003@me.com>
2019-11-30 14:39:08 +0200

    docs

--- object/integer.go
+++ object/integer.go
@@ -2,14 +2,17 @@ package object
 
 import "fmt"
 
+// Integer represents an integer value
 type Integer struct {
 	Value int64
 }
 
+// Inspect is used for debugging
 func (i *Integer) Inspect() string {
 	return fmt.Sprintf("%d", i.Value)
 }
 
+// Type returns the integer type
 func (i *Integer) Type() Type {
 	return INTEGER_OBJ
 }
