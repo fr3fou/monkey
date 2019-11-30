9c8d9179ed7bd6c1c9cda70704b5014b9603586e
Simo Aleksandrov <simo3003@me.com>
2019-11-30 15:34:20 +0200

    parse integer ltierals!

---
+++ evaluator/evaluator.go
@@ -0,0 +1,30 @@
+package evaluator
+
+import (
+	"github.com/fr3fou/monkey/ast"
+	"github.com/fr3fou/monkey/object"
+)
+
+// Eval takes in an ast.Node and evaluates it
+func Eval(n ast.Node) object.Object {
+	switch node := n.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
